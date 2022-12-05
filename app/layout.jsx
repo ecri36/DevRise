26daaefa4753781ec62370a5013a909beeb64dc4
Stephen F <smf0211@gmail.com>
2022-12-05 13:30:20 -0600

    init commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,7 @@
-import './globals.css'
-
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
+    <html>
       <body>{children}</body>
     </html>
-  )
+  );
 }
