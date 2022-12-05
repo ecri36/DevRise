eaee860307693b1395e271b319fd12bc4f7fbfe7
Stephen F <smf0211@gmail.com>
2022-12-05 13:54:11 -0600

    initial next structure

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,11 @@
+import Greeting from './(components)/Greeting';
+
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to jobhunt</h1>
+      <h1>
+        <Greeting></Greeting>
+      </h1>
     </div>
   );
 }
