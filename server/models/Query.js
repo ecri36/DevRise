765e0a9fcf481cdccfcb6f9eba2ffb8a7cd97af0
Stephen <sfadmin@goconfluent.com>
2022-12-05 23:23:10 -0600

    f implemented this commit with setup the rest of the resolvers described in the system design sheet for MVP

--- server/models/Query.js
+++ server/models/Query.js
@@ -1,10 +1,27 @@
 const Query = `
     type Query {    
         jobs(userId: Int): Jobs, 
+        user(userId: Int): User
     }
+
+    type Mutation {
+        makeJob(jobData: JobEntry): JobResponse,
+        updateJob(jobId: Int, jobData: JobEntry): JobResponse,
+        deleteJob(jobId: Int): JobResponse, 
+        signin(signinType: String, email: String, password?: String): AuthorizationResponse
+        register(registerType: String, userData: UserData): AuthorizationResponse
+    }
+
 
     type Jobs {
         jobs: [JobEntry]
+    }
+
+    type User {
+        id: Int, 
+        name: String, 
+        email: String,
+        dailyJobsApplied
     }
 
     type JobEntry {
@@ -18,6 +35,18 @@ const Query = `
         positionType: String,
         applicationData: String
     }
+
+    type JobResponse {
+        success: Boolean, 
+        updateType: String,
+        jobId: Int
+    }
+
+    type AuthorizationResponse {
+        success: Boolean, 
+        token: String
+    }
+
 `;
 
 module.exports = Query;
--- server/models/Resolvers.js
+++ server/models/Resolvers.js
@@ -2,6 +2,8 @@ module.exports = {
   Query: {
     jobs: async (_, args) => {
       const { userId } = args;
+      //TODO: Create function that gets users jobs
+      // Return dummy data for now
       return {
         jobs: [
           {
@@ -16,6 +18,64 @@ module.exports = {
             applicationData: 'String',
           },
         ],
+      };
+    },
+    user: async (_, args) => {
+      const { userId } = args;
+      // Return dummy data for now
+      return {
+        id: 1,
+        name: 'James Cyrus Bond',
+        email: 'bond@mi6.uk',
+        dailyJobsApplied: 2,
+      };
+    },
+  },
+  Mutation: {
+    makeJob: async (_, args) => {
+      //TODO: Create function that makes job
+      const { jobData } = args;
+      // Return dummy data for now
+      return {
+        success: true,
+        updateType: 'create',
+        jobId: 1,
+      };
+    },
+    updateJob: async (_, args) => {
+      //TODO: Create function that makes job
+      const { jobId, jobData } = args;
+      // Return dummy data for now
+      return {
+        success: true,
+        updateType: 'update',
+        jobId: 1,
+      };
+    },
+    deleteJob: async (_, args) => {
+      //TODO: Create function that makes job
+      const { jobId } = args;
+      // Return dummy data for now
+      return {
+        success: true,
+        updateType: 'delete',
+        jobId: 1,
+      };
+    },
+    signin: async (_, args) => {
+      const { signinType, email, password } = args;
+      // Return dummyData for now
+      return {
+        success: true,
+        token: 'dummy token',
+      };
+    },
+    register: async (_, args) => {
+      const { registerType, UserData } = args;
+      // Return dummy data for now
+      return {
+        success: true,
+        token: 'dummy token',
       };
     },
   },
