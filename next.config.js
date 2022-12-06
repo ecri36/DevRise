6da69b08a3cbc0b4623c52a119b45f0f7d933c8a
cyrusyari <cyrus.brk@gmail.com>
2022-12-06 01:03:09 +0000

    frontend beta done

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  images: {
+    domains: ['randomuser.me'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
